90d92b3cb3350bd9e8f70155b852eb11da3b6725
Yagna Srinath Reddy Battula <srinath.battula@foghorn.io>
2019-05-06 13:36:32 -0700

    added __print_chk function

--- src/gnu_extensions.c
+++ src/gnu_extensions.c
@@ -38,3 +38,14 @@ void _gfortran_concat_string (int destlen, char *dest,
 int32_t _gfortran_pow_i4_i4(int32_t val, int32_t exppon) {
   return (int32_t) pow(val, exppon);
 }
+
+int __printf_chk (int flag, const char *format, ...)
+{
+  va_list ap;
+  int done;
+  va_start (ap, format);
+  done = vfprintf (stdout, format, ap);
+  va_end (ap);
+  return done;
+}
+
